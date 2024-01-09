09009b89e46978355383c8f58595a2377d611047
Sik <contact@sikelio.wtf>
2024-01-09 19:17:40 +0100

    Separated files & Added Makefile

--- main.c
+++ main.c
@@ -1,20 +1,5 @@
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/mount.h>
-#include <sys/stat.h>
-#include <errno.h>
-
-#define CLONE_NEWPID 0x00000200
-#define CLONE_NEWNS  0x00020000
-
-typedef struct {
-    const char *sourcePath;
-    const char *destPath;
-} FilePath;
+#include "./src/headers/main.h"
+#include "./src/struct/filepath.h"
 
 int unshare(int flags);
 
---
+++ src/struct/filepath.h
@@ -0,0 +1,9 @@
+#ifndef FILEPATH_H
+#define FILEPATH_H
+
+typedef struct {
+    const char *sourcePath;
+    const char *destPath;
+} FilePath;
+
+#endif
